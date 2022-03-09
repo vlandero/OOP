ed3d1b83cfa63832c9cdb6dd57d7c91e13ad4f4d
vlandero <vlad.andrei.rosu@gmail.com>
2022-03-09 10:43:21 +0200

    Tema1

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     Zbor z1{"Paris","Roma",16,35,18,20};
     Zbor z2{"Roma","Washington",20,05,23,25};
     Bilet b{"a1",1500,1,1,1,std::vector{z1,z2}};
-    Persoana p{"Rosu","Vlad-Andrei",19,"5020712******",std::vector{b}};
+    Persoana p{"Rosu","Vlad-Andrei",19,"5020712******",std::vector{b},"werew"};
 
     std::cout<<p;
     return 0;
